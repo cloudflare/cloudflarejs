f3423da051e37843ed8b91838b27965bf5341fc0
hrushikeshdeshpande <161167942+hrushikeshdeshpande@users.noreply.github.com>
2024-08-14 13:14:33 -0400

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,3 @@
+# This is a test script with garbage secret values to test Semgrep. This is not an actual secret, please ignore! 
+# printing random garbage secret values
+print (API secret = 'hG7Z3LpQ9fXk1rBt' + JWT = 'cA2nM8vTzR5KpLJ9' + AES encryption key ='wQ4PzCm6N7XsYd5V')
